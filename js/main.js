511ecf025f0f27b0c4494cf192c8dcc0d889371d
GeorgijBugaev <georgijbugaev@gmail.com>
2021-10-01 11:54:35 +0600

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function getRandomInt(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+function checkLenght(checkedStr, maxLenght) {
+  if (str.length >= maxLenght) {
+  result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
